f8034dee0943f1bb07424209a585a91865e6537a
Rovis91 <antoine.rtc3@gmail.com>
2024-07-27 15:27:23 +0200

    Add gitignore and config.py to use .env file

--- leboncoin/config.py
+++ leboncoin/config.py
@@ -2,16 +2,22 @@
 config.py
 ---------
 
-Ce module contient les configurations spécifiques pour scraper leboncoin.fr, 
+Ce module contient les configurations spécifiques pour scraper leboncoin.fr,
 y compris les paramètres des proxies et les headers HTTP.
 """
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()  # Charger les variables d'environnement depuis le fichier .env
 
 HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
 }
+
 PROXY = {
-    'http': 'http://proxy.example.com:8080',
-    'https': 'https://proxy.example.com:8080'
+    'http': os.getenv('PROXY_HTTP'),
+    'https': os.getenv('PROXY_HTTPS')
 }
 
 # Initialisation des configurations.
